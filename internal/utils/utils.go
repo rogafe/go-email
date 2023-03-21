857e2d4a228cd63c48b576d49fd84344639d8511
rogafe <rogafe@users.noreply.github.com>
2023-03-22 00:31:02 +0100

    Modified the server.go and google oauth. server.go no do not read the secret.json, so it can be used for other oauth. google oauth test if the token is expired and ask for a reconnect

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -9,9 +9,27 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"time"
 
 	"golang.org/x/oauth2"
 )
+
+func IsAfterCurrentTime(t time.Time) bool {
+	now := time.Now()
+	current := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), 0, now.Location())
+	return t.After(current)
+}
+
+func IsTimeEqualToday(t time.Time) bool {
+	// truncate the time to midnight
+	t = t.Truncate(24 * time.Hour)
+
+	// get today's date
+	today := time.Now().Truncate(24 * time.Hour)
+
+	// compare the two dates
+	return t.Equal(today)
+}
 
 func StreamToByte(stream io.Reader) []byte {
 	buf := new(bytes.Buffer)
@@ -25,7 +43,7 @@ func StreamToString(stream io.Reader) string {
 	return buf.String()
 }
 
-//create folder
+// create folder
 func CreateFolder(folderName string) {
 
 	err := os.MkdirAll(folderName, 0755)
